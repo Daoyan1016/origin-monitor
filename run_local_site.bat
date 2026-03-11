e6af72e8c42117adc2359d6f1c8c3281b06909b7
Daoyan1016 <Daoyan1016@users.noreply.github.com>
2026-03-11 15:34:32 +0800

    Fix local startup scripts to use python

--- run_local_site.bat
+++ run_local_site.bat
@@ -2,4 +2,4 @@
 cd /d "%~dp0"
 title Origin Monitor Local Server
 echo Starting local site at http://localhost:8080/
-py -m http.server 8080
+python -m http.server 8080
--- start_sync_data.bat
+++ start_sync_data.bat
@@ -2,4 +2,4 @@
 cd /d "%~dp0"
 title Origin Monitor Data Sync
 echo Starting live data sync loop...
-py sync_live_data.py --interval 60
+python sync_live_data.py --interval 60
